7f618674aa46ad3b978cdbc47bb1349c89f69c7e
Xian Dev <xiandev25@gmail.com>
2025-11-21 13:39:16 +0100

    Set initial layout of the app UI

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
 
@@ -20,8 +24,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             LemonadeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Lemonade(
+                        title = "Lemonade",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +35,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Lemonade(title: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier
+    ) {
+        Title(title = title)
+        MainContent()
+    }
+}
+
+@Composable
+fun Title(title: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = title
     )
+}
+
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.lemon_tree),
+            contentDescription = stringResource(id = R.string.lemon_tree_image_content_description)
+        )
+        Text(
+            text = stringResource(id = R.string.lemon_tree_prompt_message)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LemonadePreview() {
     LemonadeTheme {
-        Greeting("Android")
+        Lemonade(
+            title = "Lemonade"
+        )
     }
 }
