2a0e87fbe08bb85088ca3424d44295f2d75af448
Xian Dev <xiandev25@gmail.com>
2025-11-22 22:46:00 +0100

    Center the composables on the UI

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -6,15 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +44,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Lemonade(title: String, modifier: Modifier = Modifier) {
     Column(
-        modifier
+        modifier = modifier
+            .fillMaxSize(),
     ) {
         Title(title = title)
         MainContent()
@@ -47,21 +55,33 @@ fun Lemonade(title: String, modifier: Modifier = Modifier) {
 @Composable
 fun Title(title: String, modifier: Modifier = Modifier) {
     Text(
-        text = title
+        text = title,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
     )
 }
 
 @Composable
 fun MainContent(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(id = R.drawable.lemon_tree),
-            contentDescription = stringResource(id = R.string.lemon_tree_image_content_description)
+            contentDescription = stringResource(id = R.string.lemon_tree_image_content_description),
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        Spacer(
+            modifier = Modifier
+                .height(height = 16.dp)
         )
         Text(
-            text = stringResource(id = R.string.lemon_tree_prompt_message)
+            text = stringResource(id = R.string.lemon_tree_prompt_message),
         )
     }
 }
